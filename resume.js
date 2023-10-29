8161d4cefda491b477617df603efcf7eff9bfa90
TamizhanbanT <143093806+TamizhanbanT@users.noreply.github.com>
2023-10-29 14:25:24 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,30 @@
+const Resume={
+    Name: "Tamizhanban T",
+   mail_id: "tamizhanban4563@gmail.com",
+   phone_no:9876543210,
+   languages_known:["tamil","english"],
+   qualification: [
+    {
+        school: "vethathiri maharishi higher secondary school",
+        class:"10th",
+        mark:450
+   },
+   {
+    school: "vethathiri maharishi higher secondary school",
+    class:"12th",
+    mark:963
+   },
+   {
+    college_name:"sri muthukumaran institite of technology",
+    degree:"B.E.Mechanical",
+    CGPA:6.50
+   },
+
+   ]
+};
+    console.log(Resume.qualification[2])
+
+
+
+
+
